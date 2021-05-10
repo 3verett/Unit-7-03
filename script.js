ccf46f3d05de12ad92712b58b29aa7e7f54e6411
Everett <>
2021-05-10 14:50:34 +0000

    mama

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-document.getElementById('input').addEventListener('click', ageCheck);
-let userAge = 0;
+document.getElementById('input').addEventListener('click', ageCheck)
+let userAge = 0
 
 function ageCheck () {
-  userAge = document.getElementById('age-input').value;
-  userAge = parseInt(userAge);
+  userAge = document.getElementById('age-input').value
+  userAge = parseInt(userAge)
 
   if (userAge >= 17) {
     document.getElementById('age-answer').innerHTML = 'You can see an R rated movie alone.'
-  }
-  else if (userAge >= 13) {
+  } else if (userAge >= 13) {
     document.getElementById('age-answer').innerHTML = 'You can see a PG-13 rated movie alone.'
-  }
-  else if (userAge < 13) {
+  } else if (userAge < 13) {
     document.getElementById('age-answer').innerHTML = 'You can see a G rated movie.'
-  }
-  else {
+  } else {
     document.getElementById('age-answer').innerHTML = 'Please enter a valid age.'
   }
 }
