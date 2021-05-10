579ee4c0e9b9e911fdc5a1d9ca94afc70e537493
Everett <>
2021-05-10 14:09:01 +0000

    rstjrtjrj

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function ageCheck () {
 
   if (userAge >= 17) {
     document.getElementById('age-answer').innerHTML = 'You can see an R rated movie alone.'
-  ;}
+  }
 
   else if (userAge >= 13) {
     document.getElementById('age-answer').innerHTML = 'You can see a PG-13 rated movie alone.'
-  ;}
+  }
 
   else if (userAge < 13) {
     document.getElementById('age-answer').innerHTML = 'You can see a G rated movie.';
-  ;}
+  }
 
   else {
     document.getElementById('age-answer').innerHTML = 'Please enter a valid age.';
